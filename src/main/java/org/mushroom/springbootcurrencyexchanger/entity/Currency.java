bad642aceebee3de9cf9d33c47d0c9fe36e36ce3
mrBLare <mrblreses@gmail.com>
2025-05-05 16:42:38 +0300

    added currency entity

--- src/main/java/org/mushroom/springbootcurrencyexchanger/entity/Currency.java
+++ src/main/java/org/mushroom/springbootcurrencyexchanger/entity/Currency.java
@@ -1,4 +1,42 @@
 package org.mushroom.springbootcurrencyexchanger.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "currencies")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Currency {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "sign")
+    private String sign;
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Currency currency)) return false;
+        return Objects.equals(id, currency.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
